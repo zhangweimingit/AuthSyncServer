26f60f45ce759db52fb3f2928fc8480e76ee0bc6
wmzhang <wmzhang@ikuai8.com>
2018-01-07 21:06:05 +0800

    gg

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,34 @@
 using namespace std;
 using namespace cppbase;
 namespace po = boost::program_options;
+using  boost::serialization::singleton;
 
-
+static void process_command(int argc, const char **argv);
 int main(int argc, const char **argv)
 {
+	process_command(argc, argv);
+
+	const auth_config& config = singleton<auth_config>::get_const_instance();
+
+	try
+	{
+		sync_db database(config.db_server_, config.db_user_, config.db_pwd_, config.thread_cnt_);
+		server auth_server(config.port_, config.thread_cnt_, database);
+		auth_server.run();
+	}
+	catch (const exception &e) 
+	{
+		LOG_ERRO("program exit exception:%s", e.what());
+	}
+
+	LOG_INFO("AuditSyncServer end");
+	return 0; 
+}
+
+static void process_command(int argc, const char **argv)
+{
 	po::options_description desc("Allow options");
-	
+
 	desc.add_options()
 		("help", "print help messages")
 		("config", po::value<string>()->default_value("conf/audit_sync.conf"), "Specify the config file")
@@ -33,56 +55,50 @@ int main(int argc, const char **argv)
 	{
 		po::store(po::parse_command_line(argc, argv, desc), vm);
 		po::notify(vm);
-
-		if (vm.count("help")) 
-		{
-			cout << desc << endl;
-			return 0;
-		}
-
-		auto log_file_name = vm["log"].as<string>();
-		char *dir = strdup(log_file_name.c_str());
-		char *base = strdup(log_file_name.c_str());
-		if (log_init(dirname(dir), basename(base), D_INFO, 7, 4 * 1024)) {
-			cerr << "log_init failed" << endl;
-			exit(1);
-		}
-		free(dir);
-		free(base);
-
-		LOG_INFO("AuditSyncServer start");
-		auto config_file_name = vm["config"].as<string>();
-		auth_config& config = boost::serialization::singleton<auth_config>::get_mutable_instance();
-
-		if (!config.parse(config_file_name))
-		{
-			cerr << "parse_config_file failed" << endl;
-			exit(1);
-		}
-
-		reset_log_level(config.log_level_.c_str());
-		LOG_DBUG("AuditSyncServer start 2");
-
-		if (vm.count("daemon"))
-		{
-			if (daemon(1, 1))
-			{
-				LOG_ERRO("daemon failed");
-				exit(1);
-			}
-			LOG_INFO("AuditSyncServer become a daemon service");
-		}
-
-		sync_db database(config.db_server_, config.db_user_, config.db_pwd_, config.thread_cnt_);
-		server auth_server(config.port_, config.thread_cnt_, database);
-		auth_server.run();
 	}
-	catch (const exception &e) 
+	catch (const exception &e)
 	{
-		LOG_ERRO("program exit exception:%s", e.what());
-		cout << e.what() << endl;;
+		cout << e.what() << endl;
+		cout << desc << endl;
+		exit(1);
+	}
+	if (vm.count("help"))
+	{
+		cout << desc << endl;
+		exit(0);
 	}
 
-	LOG_INFO("AuditSyncServer end");
-	return 0; 
+	string log_file_name = vm["log"].as<string>();
+	char *dir = strdup(log_file_name.c_str());
+	char *base = strdup(log_file_name.c_str());
+	if (log_init(dirname(dir), basename(base), D_INFO, 7, 4 * 1024)) 
+	{
+		cerr << "log_init failed" << endl;
+		exit(1);
+	}
+	free(dir);
+	free(base);
+
+	LOG_INFO("AuditSyncServer start");
+	string config_file_name = vm["config"].as<string>();
+	auth_config& config = singleton<auth_config>::get_mutable_instance();
+
+	if (!config.parse(config_file_name))
+	{
+		cerr << "parse_config_file failed" << endl;
+		exit(1);
+	}
+
+	reset_log_level(config.log_level_.c_str());
+	LOG_INFO("AuditSyncServer start 2");
+
+	if (vm.count("daemon"))
+	{
+		if (daemon(1, 1))
+		{
+			LOG_ERRO("daemon failed");
+			exit(1);
+		}
+		LOG_INFO("AuditSyncServer become a daemon service");
+	}
 }
